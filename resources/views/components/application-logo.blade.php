e39f376322e70588f0704b6dc99c90a1137abddf
Isaac <isaacbalabbo0819@gmail.com>
2024-07-15 15:25:40 +0800

    Generic  Logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,7 @@
 <div class="flex justify-center items-center gap-2">
-    <img src="images/logo.png" class="w-auto h-9" alt="logo">
-    <div>SPNHS</div>
+    {{-- <img src="images/logo.png" class="w-auto h-9" alt="logo">
+    <div>SPNHS</div> --}}
+
+    LOGO HERE
 </div>
 
